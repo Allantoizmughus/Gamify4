4c8fc40a5397e372720fbc1f92de76b364a8fc20
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-11 02:29:42 +0300

    Create the retrofit instamce and added the internet permission in the manifest

--- app/src/main/java/com/moringaschool/gamify/GamesClient.java
+++ app/src/main/java/com/moringaschool/gamify/GamesClient.java
@@ -1,4 +1,35 @@
 package com.moringaschool.gamify;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class GamesClient {
+    private static Retrofit retrofit=null;
+
+    public static ApiCallInterface getClient(){
+        if(retrofit == null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor(){
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://www.freetogame.com")
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+    }
 }
