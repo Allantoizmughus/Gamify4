532c4e0140b8edcb7c4165974855236323b80e6a
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-12 03:15:47 +0300

    Add inner class gameViewHolder inside the GamesListAdapter class

--- app/src/main/java/com/moringaschool/gamify/adapters/GamesListAdapter.java
+++ app/src/main/java/com/moringaschool/gamify/adapters/GamesListAdapter.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.gamify.R;
 import com.moringaschool.gamify.models.GameSearchResponse;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class GamesListAdapter extends RecyclerView.Adapter<GamesListAdapter.GamesViewHolder> {
     private List<GameSearchResponse> mGames;
@@ -39,19 +45,34 @@ public class GamesListAdapter extends RecyclerView.Adapter<GamesListAdapter.Game
     }
 
     public class GamesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        @BindView(R.id.gameImageView)
+        ImageView mGameImageView;
+        @BindView(R.id.gameNameTextView)
+        TextView mGameNameTextView;
+        @BindView(R.id.descriptionTextView) TextView mDescriptionNameTextView;
+        @BindView(R.id.dateTextView) TextView mDateTextView;
+
+        private Context mContext;
 
 
         public GamesViewHolder( View itemView) {
             super(itemView);
+            ButterKnife.bind(this,itemView);
+            mContext = itemView.getContext();
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindGames(GameSearchResponse game) {
+            mGameNameTextView.setText(game.getTitle());
+            mDescriptionNameTextView.setText(game.getShortDescription());
+            mDateTextView.setText(game.getReleaseDate());
+            Picasso.get().load(game.getThumbnail()).into(mGameImageView);
         }
 
         @Override
         public void onClick(View v) {
 
         }
-
-        public void bindGames(GameSearchResponse gameSearchResponse) {
-        }
-
     }
 }
