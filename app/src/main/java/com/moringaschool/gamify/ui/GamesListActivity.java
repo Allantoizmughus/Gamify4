34969024edf8e58531d833099c264b72d8ed4e6e
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-12 10:46:43 +0300

    Add the methods to show error and success messages

--- app/src/main/java/com/moringaschool/gamify/ui/GamesListActivity.java
+++ app/src/main/java/com/moringaschool/gamify/ui/GamesListActivity.java
@@ -1,18 +1,15 @@
 package com.moringaschool.gamify.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.moringaschool.gamify.adapters.GamesListAdapter;
 import com.moringaschool.gamify.network.ApiCallInterface;
@@ -46,14 +43,6 @@ public class GamesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_games_list);
         ButterKnife.bind(this);
 
-//        mRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                String game = ((TextView)view).getText().toString();
-//                Toast.makeText(GamesListActivity.this, game, Toast.LENGTH_LONG).show();
-//            }
-//        } );
-
         Intent intent= getIntent();
         String category = intent.getStringExtra("category");
         mCategoryView.setText("Here are all the games in Category "+category);
@@ -65,17 +54,44 @@ public class GamesListActivity extends AppCompatActivity {
         call.enqueue(new Callback<GameSearchResponse>() {
             @Override
             public void onResponse(Call<GameSearchResponse> call, Response<GameSearchResponse> response) {
+                hideProgressBar();
                 if(response.isSuccessful()){
                     games = response.body().getGames();
                     mAdapter = new GamesListAdapter(GamesListActivity.this, games);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GamesListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showGames();
 
                 }
             }
 
             @Override
             public void onFailure(Call<GameSearchResponse> call, Throwable t) {
-
+                hideProgressBar();
+                showFailureMessage();
             }
         });
     }
+
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showGames() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
 }
