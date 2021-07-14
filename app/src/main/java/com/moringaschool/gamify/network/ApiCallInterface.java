a606f110a40cbbc2d776c97985356a3be1859704
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-14 10:18:19 +0300

    Make changes on network classes

--- app/src/main/java/com/moringaschool/gamify/network/ApiCallInterface.java
+++ app/src/main/java/com/moringaschool/gamify/network/ApiCallInterface.java
@@ -1,12 +1,14 @@
 package com.moringaschool.gamify.network;
 import com.moringaschool.gamify.models.GameSearchResponse;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiCallInterface {
     @GET("games")
-    Call<GameSearchResponse> getGames(
+    Call<List<GameSearchResponse>> getGames(
     );
 }
--- app/src/main/java/com/moringaschool/gamify/network/GamesClient.java
+++ app/src/main/java/com/moringaschool/gamify/network/GamesClient.java
@@ -12,23 +12,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GamesClient {
-    private static Retrofit retrofit=null;
+    private static Retrofit retrofit;
 
     public static ApiCallInterface getClient(){
         if(retrofit == null){
-//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//                    .addInterceptor(new Interceptor(){
-//                        @Override
-//                        public Response intercept(Chain chain) throws IOException {
-//                            Request newRequest  = chain.request().newBuilder()
-//                                    .build();
-//                            return chain.proceed(newRequest);
-//                        }
-//                    })
-//                    .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(Constant.BASE_URL)
-                    //.client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
