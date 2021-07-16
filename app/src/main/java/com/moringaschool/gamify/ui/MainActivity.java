3ee74598674a06ead3b67c32edfabd40e1410090
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-16 03:17:24 +0300

    Change the input edit text to select option box

--- app/src/main/java/com/moringaschool/gamify/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/gamify/ui/MainActivity.java
@@ -7,8 +7,10 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.moringaschool.gamify.R;
@@ -18,9 +20,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.ViewCategoryButton) Button mViewCategoryButton;
-    @BindView(R.id.category) EditText mCategory;
     @BindView(R.id.AppName) TextView mAppName;
     @BindView(R.id.contacts) Button mContactsButton;
+    @BindView(R.id.planets_spinner) Spinner mSelect;
 
 
     @Override
@@ -29,15 +31,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+
+        ArrayAdapter<String> myAdapter =new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.cate));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        mSelect.setAdapter(myAdapter);
+
         Log.e("TAG","Before onClick");
         mViewCategoryButton.setOnClickListener(this);
         mContactsButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == mViewCategoryButton) {
-            String category = mCategory.getText().toString();
+            String category = mSelect.getSelectedItem().toString();
             Intent intent = new Intent(MainActivity.this, GamesListActivity.class);
             intent.putExtra("category",category);
             Log.e("TAG","Before startActivity");
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }else if(v == mContactsButton){
             Intent intent = new Intent(MainActivity.this, ContactsActivity.class);
             startActivity(intent);
+        }else if(v== mSelect){
         }
     }
 }
