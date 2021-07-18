da8a8d3fa9652562f56dfc0f19110fcf8c35d5ea
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-18 13:56:45 +0300

    Add another constant that will be saving the category searched by users

--- app/src/main/java/com/moringaschool/gamify/Constant.java
+++ app/src/main/java/com/moringaschool/gamify/Constant.java
@@ -3,4 +3,6 @@ package com.moringaschool.gamify;
 public class Constant {
     public static final String BASE_URL="https://www.freetogame.com";
     public static final String PREFERENCES_CATEGORY_KEY = "category";
+    public static final String FIREBASE_CHILD_SEARCHED_CATEGORY = "searchedCategory";
+
 }
