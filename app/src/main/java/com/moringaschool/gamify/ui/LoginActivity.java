0e5a5fbffd8deeac16f7db48f8ebb6631416e7d4
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-22 14:47:57 +0300

    Add the firebase authentication for user registration

--- app/src/main/java/com/moringaschool/gamify/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/gamify/ui/LoginActivity.java
@@ -2,15 +2,35 @@ package com.moringaschool.gamify.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.gamify.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.signUpTextView)
+    TextView mSignUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mSignUpTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSignUpTextView){
+            Intent intent = new Intent(LoginActivity.this,CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
