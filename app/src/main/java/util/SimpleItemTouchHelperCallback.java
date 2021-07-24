509316287ea6dab79122dc966447b3025cd70005
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-25 02:18:22 +0300

    Add the the methods from the inerface ItemTouchHelper

--- app/src/main/java/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/util/SimpleItemTouchHelperCallback.java
@@ -1,4 +1,34 @@
 package util;
 
-public class SimpleItemTouchHelperCallback {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperAdapter mAdapter;
+    //  This constructor takes an ItemTouchHelperAdapter parameter. When implemented in
+    //  FirebaseGamesListAdapter, the ItemTouchHelperAdapter instance will pass the gesture event back to the
+    //  Firebase adapter where we will define what occurs when an item is moved or dismissed.
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
+        this.mAdapter = adapter;
+    }
+
+
+    @Override
+    public int getMovementFlags(@NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder) {
+        return 0;
+    }
+
+    @Override
+    public boolean onMove(@NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, @NonNull @NotNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
 }
